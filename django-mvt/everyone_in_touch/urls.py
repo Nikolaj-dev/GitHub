99c957adf15d9b3d13b3192e5c3e0d53d70f2ccd
Nickolaj <nikolajpetrishchev@gmail.com>
2023-03-05 01:29:00 +0600

    Add authorisation system(+html)

--- django-mvt/everyone_in_touch/urls.py
+++ django-mvt/everyone_in_touch/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.startpage, name='startpage'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('login/', views.login_, name='login'),
+    path('logout/', views.logout_, name='logout'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+
 ]
--- django-mvt/everyone_in_touch/views.py
+++ django-mvt/everyone_in_touch/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import User
 
 
 def startpage(request: HttpRequest) -> HttpResponse:
     return HttpResponse('<h1>You see the startpage.</h1>')
+
+
+def homepage(request):
+    return render(request, 'everyone_in_touch/homepage.html')
+
+
+def login_(request):
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect(reverse('homepage', args=()))
+        else:
+            raise Exception("Login or password is incorrect!")
+    return render(request, 'everyone_in_touch/login.html', context={})
+
+
+def sign_up(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect(reverse('homepage', args=()))
+    return render(request, 'everyone_in_touch/sign_up.html', context={})
+
+
+def logout_(request):
+    logout(request)
+    return redirect(reverse('homepage', args=()))
